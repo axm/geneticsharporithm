1ff0231dc28ef011a1d42cda581eac0e89483e4b
Alexander Mocanu <amocanu2000@gmail.com>
2015-08-01 18:43:00 +0100

    added selection step

---
+++ GeneticSharporithm/ISelection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticSharporithm
+{
+    /// <summary>
+    /// Represents the selection step of a genetic algorithm.
+    /// </summary>
+    public interface ISelection<V>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="chromosomes"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// <paramref name="chromosomes"/> must have at least one element.
+        /// </remarks>
+        IList<Chromosome<V>> Select(IList<Chromosome<V>> chromosomes);
+    }
+}
