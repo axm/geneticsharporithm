e3c85db46bc21165b61b504d049c5fbfbc60591a
Alexander Mocanu <amocanu2000@gmail.com>
2015-08-02 01:39:59 +0100

    added solution

---
+++ GeneticSharporithmProgram/Solution.cs
@@ -0,0 +1,17 @@
+﻿using GeneticSharporithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticSharporithmProgram
+{
+    public class Solution : ISolution<string>
+    {
+        public bool IsSolution(Chromosome<string> chromosome)
+        {
+            return chromosome.Genes == "abracadabra";
+        }
+    }
+}
