aab423f9016498d06217168a1970b010f4bf9f16
Alexander Mocanu <amocanu2000@gmail.com>
2015-08-08 11:04:21 +0300

    added docs to StringGenerator

--- GeneticSharporithm/StringGenerator.cs
+++ GeneticSharporithm/StringGenerator.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GeneticSharporithm
 {
-    public class StringGenerator : IPopulationGenerator<string>
+    /// <summary>
+    /// Used to generate random strings of a given length.
+    /// 
+    /// This class cannot be inherited.
+    /// </summary>
+    public sealed class StringGenerator : IPopulationGenerator<string>
     {
+        public readonly int Length;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringGenerator"/> class
+        /// using the specified length for the generated strings.
+        /// </summary>
+        /// <param name="length">
+        /// The length of the strings to be generated with
+        /// this generator.
+        /// </param>
+        /// <remarks>
+        /// Throws <see cref="ArgumentOutOfRangeException"/> if length is zero or negative.
+        /// </remarks>
+        public StringGenerator(int length)
+        {
+            Contract.Requires<ArgumentOutOfRangeException>(length > 0);
+
+            Length = length;
+        }
+        
         public string Generate()
         {
             throw new NotImplementedException();
