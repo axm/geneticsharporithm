129ea39fc9a74a0a89c60200497fe9fe46ad7d24
Alexander Mocanu <amocanu2000@gmail.com>
2015-09-01 21:11:10 +0100

    added test project for Sample code / Program project

---
+++ tests/GeneticAlgorithmProgramTests/Selectors/UniqueChromosomeSelectorTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using GeneticSharporithmProgram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticSharporithmProgram.Tests
+{
+    [TestFixture()]
+    public class UniqueChromosomeSelectorTests
+    {
+        [Test()]
+        public void SelectTest()
+        {
+
+        }
+    }
+}
